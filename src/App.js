6c9935545fe5ea47e236917aa0a596a20197f431
ThePupas <37349644+ThePupas@users.noreply.github.com>
2023-08-26 22:04:16 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'mini.css';
+import FormFiled from './components/FormField/FormFiled';
+
+function App() {
+  return (
+    <div className='container'>
+      <FormFiled />
+    </div>
+  );
+}
+
+export default App;
